dd3c87a885228b10fda0610dd7c8a48e86a6d5c1
KhinMyintMyatThu <kmmt96@gmail.com>
2019-12-05 19:55:13 +0630

    Google places file first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,84 @@
+<!-- <div id="mapDiv" style="width: 100%; height: 400px"> </div>
+<input id="latitude" name="latitude" type="text">
+<input id="longitude" name="longitude" type="text">
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyB9ayIX4awOPtN9t78B_OLQ_jROkBpcJ_E&callback=initMap"
+type="text/javascript"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script type="text/javascript">
+    window.onload = function () {
+        var mapOptions = {
+            center: new google.maps.LatLng(16.798307, 96.149612),
+            zoom: 14,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var infoWindow = new google.maps.InfoWindow();
+        var latlngbounds = new google.maps.LatLngBounds();
+
+        //This will load your map with default location co-ordinates.
+        var map = new google.maps.Map(document.getElementById("mapDiv"), mapOptions);
+
+        //To capture click event.
+        google.maps.event.addListener(map, 'click', function (e) {
+            document.getElementById("latitude").value = e.latLng.lat();
+            document.getElementById("longitude").value = e.latLng.lng();
+            placeMarker(e.latLng, map);
+
+            // var response = function("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + e.latLng.lat() + "," + e.latLng.lng() + "&radius=1000&type=restaurant&keyword=cruise&key=AIzaSyB9ayIX4awOPtN9t78B_OLQ_jROkBpcJ_E"){
+            //     $.ajax({
+            //         url: 'welcome.blade.php',
+            //         type: 'POST',
+            //         data: {id:id},
+            //         success: function(data) {
+            //             console.log(data); // Inspect this in your console
+            //         }
+            //     });
+            // }
+
+            var placesURL= "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + e.latLng.lat() + "," + e.latLng.lng() + "&radius=1000&type=restaurant&keyword=cruise&key=AIzaSyB9ayIX4awOPtN9t78B_OLQ_jROkBpcJ_E";
+           // var json_obj = JSON.parse(Get(placesURL));
+            console.log("The places url is "+placesURL);
+            // console.log("this is the author name: " + JSON.stringify(json_obj));
+            // $.post("/foo/", JSON.stringify(json_obj), function(temp){
+            //     console.log(temp);
+            // });
+         //   fs.writeFile("places.json",JSON.stringify(json_obj));
+        });
+    }
+    //Adding marker on click
+    var marker;
+    function placeMarker(location, map) {
+        if (marker) {
+            marker.setPosition(location);
+        } else {
+            marker = new google.maps.Marker({
+                position: location,
+                map: map
+            });
+        }
+    }
+
+    //Function for Http get request
+    function Get(yourUrl) {
+        var Httpreq = new XMLHttpRequest(); // a new request
+        Httpreq.open("GET", yourUrl, false);
+        Httpreq.send(null);
+        return Httpreq.responseText;
+    }
+</script> -->
+
+<?php
+ use SKAgarwal\GoogleApi\PlacesApi;
+ use Illuminate\Support\Facades\Storage;
+
+  error_log("hello");
+  $googlePlaces = new PlacesApi('AIzaSyB9ayIX4awOPtN9t78B_OLQ_jROkBpcJ_E');
+  $response = $googlePlaces->placeAutocomplete('Restaurants'); 
+  $response1 = $googlePlaces->nearbySearch('16.79659461262182,96.15240933445489','1000');
+ // Storage::disk('public')->put('restaurant.json', response()->json($response));
+//   echo $response;
+//   $json_output = json_decode($response1);
+//   print_r($json_output);
+echo $response1;
+
+?>
